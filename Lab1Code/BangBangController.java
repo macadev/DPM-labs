82f0a8ab61aad3760fe7d9ea9c2c05fc19626b1b
macadev <danielmacario5@gmail.com>
2014-09-11 23:06:31 -0400

    bangbang solution for open corners :+1:

--- Lab1Code/BangBangController.java
+++ Lab1Code/BangBangController.java
@@ -4,10 +4,15 @@ import lejos.nxt.comm.RConsole;
 
 public class BangBangController implements UltrasonicController{
 	private final int bandCenter, bandwith;
-	private final int motorLow = 50, motorHigh=200;
-	private final int motorStraight = 150;
+	private final int motorLow = 50, motorHigh = 400;
+	private final int motorStraight = 200;
+	private final int TURNLEFT = 1;
+	private final int TURNRIGHT = 0;
+	
+	
 	private final NXTRegulatedMotor leftMotor = Motor.A, rightMotor = Motor.C;
 	private int distance;
+	private int previousDistance = 20;
 	private int currentLeftSpeed;
 	
 	public BangBangController(int bandCenter, int bandwith, int motorLow, int motorHigh) {
@@ -27,22 +32,66 @@ public class BangBangController implements UltrasonicController{
 	public void processUSData(int distance) {
 		this.distance = distance;
 		// TODO: process a movement based on the us distance passed in (BANG-BANG style)
-        if (this.distance < (this.bandCenter-this.bandwith)){
+		
+		//if previous measurement is much smaller than the current one, run corner protocol
+		/*
+		if (Math.abs(distance - this.previousDistance) > 20) {
+			RConsole.println("gap");
+			openCornerTurn();
+			this.distance = this.previousDistance;
+		}
+		*/
+		
+		//save previous distance - used to detect corners
+		this.previousDistance = distance;
+        if (this.distance < (this.bandCenter - this.bandwith)){
 			//Too close to the wall, slow down outside wheel
-			rightMotor.setSpeed(motorStraight);
-			leftMotor.setSpeed(motorHigh);
-
-		} else if (this.distance > (this.bandCenter+this.bandwith)) {
+			leftFaster();
+		} else if (this.distance > (this.bandCenter + this.bandwith)) {
 			//Too far from the wall, slow down inside wheel
-			leftMotor.setSpeed(motorStraight);
-			rightMotor.setSpeed(motorHigh);
+			rightFaster();
         } else {
-			// we assume that the value is in the center band as it is not above or below
-			rightMotor.setSpeed(motorStraight);
-			leftMotor.setSpeed(motorStraight);
+			// Robot is within bandwith
+			bothStraight();
         }
-        RConsole.println("Distance: "+String.valueOf(this.distance)+'\n'+"Speed: L->"+String.valueOf(leftMotor.getSpeed())+
-                " R->"+String.valueOf(rightMotor.getSpeed()));
+        //RConsole.println("Distance: "+String.valueOf(this.distance)+'\n'+"Speed: L->"+String.valueOf(leftMotor.getSpeed())+
+          //      " R->"+String.valueOf(rightMotor.getSpeed()));
+	}
+
+	
+	public void openCornerTurn() {
+		bothStraight();
+		leftMotor.rotate(800, true);
+		rightMotor.rotate(800);
+		ninetyDegreeTurn(this.TURNLEFT);
+		leftMotor.rotate(800, true);
+		rightMotor.rotate(800);
+		return;
+	}
+	
+	public void ninetyDegreeTurn(int turnDirection) {
+		if (turnDirection == 0){
+			leftMotor.rotate(320, true);
+			rightMotor.rotate(-320);
+		} else {
+			leftMotor.rotate(-320, true);
+			rightMotor.rotate(320);
+		}
+	}
+	
+	public void bothStraight() {
+		rightMotor.setSpeed(motorStraight);
+		leftMotor.setSpeed(motorStraight);
+	}
+	
+	public void rightFaster() {
+		leftMotor.setSpeed(motorStraight);
+		rightMotor.setSpeed(motorHigh);
+	}
+	
+	public void leftFaster() {
+		rightMotor.setSpeed(motorStraight);
+		leftMotor.setSpeed(motorHigh);
 	}
 
 	@Override
